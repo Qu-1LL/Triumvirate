b320238e8db9cf70b1795517abb2dbaf251e5a35
xXSpaghett69Xx <qjm412@gmail.com>
2024-12-22 15:08:47 -0500

    made Action.js

---
+++ game/Action.js
@@ -0,0 +1,34 @@
+class Action {
+
+    constantActions = [
+        "Income",
+        "Foreign Aid",
+        "Coup"
+    ];
+
+    senateActions = [
+        "Elect",
+        "Impeach",
+        "Embezzle"
+    ];
+
+    cardActions = [
+        "Steal",
+        "Tax",
+        "Exchange",
+        "Assassinate"
+    ];
+
+    doConstantAction(index) {
+
+    }
+
+    doSenateAction(index) {
+
+    }
+
+    doCardAction(index) {
+        
+    }
+
+}
