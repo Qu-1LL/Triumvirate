c010c206392d7e2fbbe85991f582429023d2c2ba
Noah Shin <nds2529@g.rit.edu>
2024-12-21 19:30:47 -0500

    Created Triumvirate, implemented Deck, Senate, and Treasury

---
+++ game/Triumvirate.js
@@ -0,0 +1,18 @@
+class Triumvirate{ 
+
+    cardTypes = [
+        "Captain",
+        "Ambassador",
+        "Duke",
+        "Contessa",
+        "Assassin"
+    ];
+
+    constructor(influences){
+        //const cardTypeDict = {};
+        this.initializeCardTypes();
+        this.influences = influences;
+    }
+
+    
+}
