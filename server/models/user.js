da859a689d18c21bc34cfa7efc88fca46d10f2f7
CeffreyJostello <90864809+CeffreyJostello@users.noreply.github.com>
2024-12-26 20:44:37 -0600

    added user schema

---
+++ server/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const {v4: uuidv4} = require('uuid');
+
+const playerSchema = new mongoose.Schema({
+    uid: {
+        type: uuidv4,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    wins: {
+        type: Number,
+        default: 0
+    },
+    losses: {
+        type: Number,
+    }
+})
