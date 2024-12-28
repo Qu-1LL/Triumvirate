518de2e30fde8cb74152bfee682be944b1989fd6
CeffreyJostello <90864809+CeffreyJostello@users.noreply.github.com>
2024-12-27 22:58:57 -0600

    http.get

--- server/index.js
+++ server/index.js
@@ -1,23 +1,22 @@
 require('dotenv').config;
 
-const triumvirate_app = require('express')();
+const express = require('express');
+const triumvirate_app = express();
 const bcrypt = require('bcrypt');
 const jsonwebtoken = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const socketio = require('socket.io')()
+
 triumvirate_app.use(express.json());
+const PORT = 3000
+triumvirate_app.listen(
+  PORT,
+  () => console.log(`Trimvirate Server Running on http://localhost:${PORT}`)
+);
 
-triumvirate_app.use(cors({
-    origin: 'http://localhost:2400',
-    methods: ['GET', 'PUT', 'POST', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
-triumvirate_app.get('/', (req, res) => {
-  res.send('Hello, Express!');
-});
-
-triumvirate_app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+triumvirate_app.get('/rooms', (req, res) => {
+  res.status(200).json({
+    message: 'Hello world'
+  })
 });
