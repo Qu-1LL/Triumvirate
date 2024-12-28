350526557f4e9813286ece2967729f857b3a3dbb
CeffreyJostello <90864809+CeffreyJostello@users.noreply.github.com>
2024-12-27 21:40:40 -0600

    added socket.io client package to frontend

--- triumvirate-app/src/app/app.component.ts
+++ triumvirate-app/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'triumvirate-app';
+  title = 'skibidi sigma balls cop';
 }
