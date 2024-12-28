f06f3eaf8bdfe030352dd991a4a8ca8bb1de5e40
xXSpaghett69Xx <qjm412@gmail.com>
2024-12-27 23:57:48 -0500

    seperate page for lobby

---
+++ triumvirate-app/src/app/rooms-page/rooms-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rooms-page',
+  imports: [],
+  templateUrl: './rooms-page.component.html',
+  styleUrl: './rooms-page.component.css'
+})
+export class RoomsPageComp {
+  title = 'Lobby'
+}
